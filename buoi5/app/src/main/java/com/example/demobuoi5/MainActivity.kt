6c7512debaf8b93bccc79f79e3e8b1d362a07997
ChungHa136 <hackusavn@gmail.com>
2023-05-25 22:17:44 +0700

    Add layout

--- buoi5/app/src/main/java/com/example/demobuoi5/MainActivity.kt
+++ buoi5/app/src/main/java/com/example/demobuoi5/MainActivity.kt
@@ -10,20 +10,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                add<FirstFragment>(
-                    containerViewId = R.id.container,
-                    tag = "FirstFragment",
-                    args = bundleOf(
-                        "key1" to 1,
-                        "key2" to "hello",
-                        "key3" to arrayListOf("1", "2", "3")
-                    ),
-                )
-            }
-        }
+//
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.commit {
+//                setReorderingAllowed(true)
+//                add<FirstFragment>(
+//                    containerViewId = R.id.container,
+//                    tag = "FirstFragment",
+//                    args = bundleOf(
+//                        "key1" to 1,
+//                        "key2" to "hello",
+//                        "key3" to arrayListOf("1", "2", "3")
+//                    ),
+//                )
+//            }
+//        }
     }
 }
