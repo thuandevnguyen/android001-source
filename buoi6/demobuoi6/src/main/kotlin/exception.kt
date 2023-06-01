80352cc7ac40a123720cf008ded8a8984579f46f
hoc081098@gmail.com <hoc081098@gmail.com>
2023-06-01 23:17:55 +0700

    lec 6 - exception

--- buoi6/demobuoi6/src/main/kotlin/exception.kt
+++ buoi6/demobuoi6/src/main/kotlin/exception.kt
@@ -15,22 +15,28 @@ fun main(): Unit = runBlocking {
         + CoroutineExceptionHandler { coroutineContext, throwable ->
       println("In $coroutineContext")
       println("Catch $throwable")
+      println(throwable.suppressedExceptions)
     }
   )
   
   scope.launch {
     println("START 1")
-    throw RuntimeException("Broken!!")
+    throw RuntimeException("Broken 1!!")
   }
   
   scope.launch {
-    delay(100)
-    println("DONE 2")
+    println("START 2")
+    throw RuntimeException("Broken 2!!")
   }
   
   scope.launch {
     delay(100)
     println("DONE 3")
+  }
+  
+  scope.launch {
+    delay(100)
+    println("DONE 4")
   }
   
   delay(1000)
