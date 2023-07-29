1bd627779765638cf109a09e6dd1cbc187c48c37
ChungHa136 <hackusavn@gmail.com>
2023-07-29 21:30:22 +0700

    Add demo navigation

---
+++ buoi9/lecture9-sample/app/src/main/java/com/rxmobileteam/lecture9sample/BlankFragment3.kt
@@ -0,0 +1,40 @@
+package com.rxmobileteam.lecture9sample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.rxmobileteam.lecture9sample.databinding.FragmentBlank3Binding
+import com.rxmobileteam.lecture9sample.databinding.FragmentBlankBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BlankFragment3.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class BlankFragment3 : Fragment() {
+  private var _binding: FragmentBlank3Binding? = null
+  private val binding get() = _binding!!
+
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    _binding = FragmentBlank3Binding.inflate(inflater, container, false)
+    return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    binding.tvNavigationFragment3.setOnClickListener {
+      findNavController().popBackStack(destinationId = R.id.blankFragment, inclusive = false)
+    }
+  }
+}
